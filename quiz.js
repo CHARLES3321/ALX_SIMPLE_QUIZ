55dc86dd7545c79b79a743afd3c5ed6862925fd9
CHARLES3321 <okekecharles363@gmail.com>
2024-10-14 15:50:14 +0100

    second commit

--- quiz.js
+++ quiz.js
@@ -1,15 +1,15 @@
-function checkAnswer() {
-    const correctAnswer = "4"
+function checkAnswer(userAnswer) {
+    const correctAnswer = "4";
 
     const userAnswer=document.querySelector('input[name="quiz"]:checked').value;
 
     const feedbackElement=
-    document.getElementByIfunction
+    document.getElementById("feedback");
 
     if (userAnswer === correctAnswer) {
         feedbackElement.textContent= "Correct!Well done.";
     }   else {
-        feedbackElement.textContent="That's incoorect. Try again!";
+        feedbackElement.textContent="That's incorrect. Try again!";
     }
 }
 
