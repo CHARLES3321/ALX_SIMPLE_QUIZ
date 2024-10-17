de9f0afae09520753843cbbe5956f994933df359
CHARLES3321 <okekecharles363@gmail.com>
2024-10-17 23:24:48 +0100

    adding

--- calculator.js
+++ calculator.js
@@ -41,4 +41,24 @@ function performCalculation(operation) {
 
     const number2=parseFloat(document.getElementById(`number2`).value)||0;
  
+
+
+let result;
+switch(operation) { 
+    case `add`:
+    result=add(number1,number2);
+    break;
+    case `subtract`:
+    result=subtract(number1,number2);
+    break;
+    case `multiply`:
+    result=multiply(number1,number2);
+    break;
+    case `divide`:
+    result=divide(number1,number2);
+    break;
+    default:
+        result="Invalid operation";
+}
+document.getElementById(`calculation-result`).textContent=result;
 }
